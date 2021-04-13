c73673bb103d9ca40966a5f237b27aee0c4ca42c
Shiv-tec <80725836+Shiv-tec@users.noreply.github.com>
2021-04-13 12:19:59 +1000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HelloWorld2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                string user1 = "Shiv_p24";
+
+            Console.WriteLine("Enter username:");
+                string user2 = Console.ReadLine();
+            
+                string pass1 = "prasad";
+            if( user1 != user2){
+                Console.WriteLine("Enter Password:");
+                Console.ReadLine();
+                Console.WriteLine("Login Unsucessful");
+            }
+            
+            if( user1 == user2){
+            Console.WriteLine("Enter Password:");
+                    string pass2 = Console.ReadLine();
+                if( pass1 == pass2){
+                Console.WriteLine("Login Sucessful");}
+            else {
+                Console.WriteLine("Login Unsucessful");
+            }
+           
+            
+        }
+    }
+}
+}
